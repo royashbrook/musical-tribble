f8e6fac8e9dbc4d91f7ff7b039ee60eb05926414
Roy <roy@ashbrook.io>
2022-05-12 16:58:11 -0400

    update graph config for sp repo

--- src/stores/graph/graph.config.js
+++ src/stores/graph/graph.config.js
@@ -1,7 +1,7 @@
 //https://docs.microsoft.com/en-us/graph/api/resources/driveitem?view=graph-rest-1.0
 const URI = 'https://graph.microsoft.com/v1.0/drives'
-const drive = 'b!UYlwtP7ji0euGnyoEVSB7CwNHVlEd-lFq0UFOjKo34eZk2vhVjpJTqRPC91hMyIA'
-const item = '01XMKSJ4SHY645HMG6HJGLAI3BNS2V3PRK'
+const drive = 'b!cgsRbRgvX0-zTS5CPVyZGFVQzkkRmOxIpWJfTr6WB9muzopQ456cSLr_cHWuNw72'
+const item = '012AUK3FLNKPHCWRFXBFAJH6NC3FCNOSUW'
 const sel = '$select=name,lastModifiedDateTime,content.downloadUrl'
 export const graphContentEndpoint =
     `${URI}/${drive}/items/${item}/children?${sel}`
