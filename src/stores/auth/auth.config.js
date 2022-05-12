37ad08692d4998698f2ec308bbded45f95319418
Roy <roy@ashbrook.io>
2022-05-12 15:29:30 -0400

    set  clientid

--- src/stores/auth/auth.config.js
+++ src/stores/auth/auth.config.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
     auth: {
-        clientId: "debb7fe3-a91e-4e28-a033-34578c306da0",
+        clientId: "b1babd89-d925-47f7-ad78-2c7e2d36b1a2",
         authority: "https://login.microsoftonline.com/10e6cb33-238d-4dba-91a7-6f6297df795a",
         redirectUri: location.origin,
     },
