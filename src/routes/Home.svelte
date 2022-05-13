a081f3b2c11f526aec212297e2b7eea5d9962a7a
Roy <roy@ashbrook.io>
2022-05-13 07:59:05 -0400

    add search with cosmos

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,10 +1,47 @@
 <script>
-    import { accessToken, user } from '../stores/auth/auth'
-    import { getData, data} from '../stores/graph/data';
+  import { data as secrets } from '../stores/graph/data'
+  import { CosmosClient } from '@azure/cosmos'
+  $: $secrets && console.log('$secrets: ', JSON.stringify($secrets, null, 2))
+  let searchString
+  let searchResults = {}
+  const getSearchResults = async () => {
+
+    //get cosmos settings and set query
+    let connectionString = $secrets.cosmosConnection
+    let endpoint = connectionString.match(/AccountEndpoint=([^;]*);/)[1]
+    let key = connectionString.match(/AccountKey=([^;]*);/)[1]
+    let databaseId = $secrets.cosmosDatabase
+    let containerId = $secrets.cosmosContainer
+    let qry = $secrets.cosmosQuery.replace(/@p1/,`'%${searchString}%'`)
+    // searchResults = 'Searched for ' + searchString + ' - ' + Math.random()
+
+    //create client
+    const client = new CosmosClient({ endpoint, key })
+
+    //set db and container
+    const database = client.database(databaseId)
+    const container = database.container(containerId)
+    await container.items
+      .query(qry)
+      .fetchAll()
+      .then(({ resources: items }) => searchResults = items)
+      .catch((err) => console.log('error', err.message))
+  }
 </script>
 
-<h2>Welcome, {$user}!</h2>
-<h4>Congratulations! You got a token!</h4>
-<p><code>{$accessToken?.substring(0,20) + '...'}</code></p>
-<h4>Congratulations! You used the token to get secrets!</h4>
-<pre width="100%">{JSON.stringify($data,null,2)}</pre>
+<h4>Document Search:</h4>
+<input
+  placeholder="Type a value to search and hit enter."
+  bind:value={searchString}
+  on:change={getSearchResults} />
+
+{#if searchResults.length > 0}
+    <h5>Results:</h5>
+    {#each searchResults as searchResult}
+        <pre width="100%">{JSON.stringify(searchResult, null, 2)}</pre>
+    {/each}
+{:else}
+    <h5><code>No search results</code></h5>
+{/if}
+
+
